81b27cd452c771fc76a16ec4983de563e8b580b7
yogeshgirde <yogeshgirde21@gmail.com>
2020-05-07 09:07:29 +0530

    first commitss

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const schema =require("./schema")
+const schema = require("./schema");
+const cors = require("cors");
 
 const app = express();
+//Allow Cross Origin
+app.use(cors());
+
 app.use(
   "/graphql",
   graphqlHTTP({
